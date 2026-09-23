14aebcc119a54f7e5564545c7a03ef5ceaddf633
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-23 18:23:19 +0500

    feat: add ServiceCard component to display service details

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,15 @@
+function ServiceCard() {
+  return (
+    <div>
+      <h4>Frontend Development</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        quibusdam in molestias cumque facere vero dolor porro modi. Aliquam,
+        provident?
+      </p>
+      <a href="#">Check Service</a>
+    </div>
+  );
+}
+
+export default ServiceCard;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,12 @@
+import ServiceCard from "../components/ServiceCard";
+
 function Services() {
   return (
     <div>
       <h1>Our Services</h1>
       <p>Website Development and Digital Marketing.</p>
+
+      <ServiceCard />
     </div>
   );
 }
